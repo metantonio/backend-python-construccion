96592a63a501401bbb08a0ca107a6d78b1963c44
Antonio Martinez <antonio_martinez88@hotmail.com>
2022-09-06 09:54:23 -0400

    updated

--- src/rutas/tecnico.py
+++ src/rutas/tecnico.py
@@ -12,8 +12,24 @@ URL_BACKEND = os.environ.get('BASE_URL')
 @app.route('/tecnico', methods=['POST'])
 def fetching():
     data = request.get_json()
-    if isinstance(data, dict):        
-        return jsonify({"msg":"Estructura correcta de data"}), 200
+    if isinstance(data, dict):  
+        if(data["metodo"]=="POST"):
+            f = requests.post(URL_BACKEND+data["endpoint"], json={})
+    
+            answer = f.json()
+            print("existen "+str(len(answer))+" registros en: "+data["endpoint"])
+    
+            items = answer
+
+   
+            save_path = '../../BD_Prueba/'   
+            completeName = os.path.join(save_path, data["archivo"]) 
+  
+
+            with open(completeName, "w") as text_file:
+                json.dump(items, text_file, indent = 6)
+
+            return jsonify({"msg":"Data Grabada Correctamente"}), 200
     else:
         return jsonify({"msg":"error en la data"}), 400
 
